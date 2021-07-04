b758f262892c65241d43858dd602a1771fae42a5
eshwarne <northeasteditsofficial@gmail.com>
2021-07-04 19:56:42 +0530

    initial commit

---
+++ src/main/java/com/eshwar/gactiongcp/GactiongcpApplication.java
@@ -0,0 +1,13 @@
+package com.eshwar.gactiongcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GactiongcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GactiongcpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eshwar/gactiongcp/GactiongcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eshwar.gactiongcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GactiongcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
